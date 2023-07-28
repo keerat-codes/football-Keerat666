f61def80943be496a416575955f1d6c7dc7c39ad
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-28 22:26:26 +0530

    Updated App.js for playerNames route using AutoPilot

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 8009;
 
 require('dotenv').config();
 
+const playerNamesRoute = require('./routes/playerNames');
+app.use('/playerNames', playerNamesRoute);
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
